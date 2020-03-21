dc69ae654d2f9837a51754307eaedb1c4c454412
Oriol Borrell Roig <oriolborrellroig@gmail.com>
2020-03-21 16:52:19 +0100

    Working with pills

--- app/src/main/java/com/example/asifkhan/customlistview/activities/MainActivity.java
+++ app/src/main/java/com/example/asifkhan/customlistview/activities/MainActivity.java
@@ -10,39 +10,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.asifkhan.customlistview.R;
 import com.example.asifkhan.customlistview.adapters.CustomListAdapter;
+import com.example.asifkhan.customlistview.models.Pill;
 import com.example.asifkhan.customlistview.models.UserInfo;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<UserInfo> userInfos;
+    //private ArrayList<UserInfo> userInfos;
     private CustomListAdapter customListAdapter;
     private ListView customListView;
-    private String[] names={
-            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan",
-            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan",
-            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan",
-            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan",
-            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan"
-    };
-    private String[] professions={
-            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst",
-            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst",
-            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst",
-            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst",
-            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst"
-    };
-    private int[] photos={
-            R.drawable.sample_5,R.drawable.sample_1,R.drawable.sample_6,R.drawable.sample_5,R.drawable.sample_5,
-            R.drawable.sample_0,R.drawable.sample_2,R.drawable.sample_3,R.drawable.sample_5,R.drawable.sample_2,
-            R.drawable.sample_6,R.drawable.sample_1,R.drawable.sample_4,R.drawable.sample_6,R.drawable.sample_4,
-            R.drawable.sample_0,R.drawable.sample_3,R.drawable.sample_3,R.drawable.sample_5,R.drawable.sample_4,
-            R.drawable.sample_7,R.drawable.sample_1,R.drawable.sample_5,R.drawable.sample_5,R.drawable.sample_0
-    };
+    private Pill pill1 = new Pill("Pill1");
+    private Pill pill2 = new Pill("Pill2");
+    private Pill pill3 = new Pill("Pill3");
+    private ArrayList<Pill> arrayListPills;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,26 +33,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
         customListView=(ListView)findViewById(R.id.custom_list_view);
-        userInfos=new ArrayList<>();
-        customListAdapter=new CustomListAdapter(userInfos,this);
+
+        arrayListPills = new ArrayList<>();
+        arrayListPills.add(pill1);
+        arrayListPills.add(pill2);
+        arrayListPills.add(pill3);
+
+        customListAdapter=new CustomListAdapter(arrayListPills,this);
         customListView.setAdapter(customListAdapter);
-        getDatas();
+
         customListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(getApplicationContext(), ViewPillActivity.class);
-                intent.putExtra("PILL_NAME", names[i]);
-                intent.putExtra("PILL_DATE", professions[i]);
+                //intent.addFlags();
+                intent.putExtra("PILL_NAME", arrayListPills.get(i).getName());
+                intent.putExtra("PILL_DATE", arrayListPills.get(i).getName());
                 startActivityForResult(intent, 0);
             }
         });
-    }
-
-    // getting all the datas
-    private void getDatas(){
-        for(int count=0;count<names.length;count++){
-            userInfos.add(new UserInfo(names[count],professions[count],photos[count]));
-        }
     }
 
     @Override
@@ -86,15 +68,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextChange(String newText) {
                 newText=newText.toString();
-                ArrayList<UserInfo> newUserInfos=new ArrayList<>();
-                for(UserInfo userInfo:userInfos){
-                    String name=userInfo.getName().toLowerCase();
-                    String profession=userInfo.getProfession().toLowerCase();
-                    if(name.contains(newText) || profession.contains(newText)){
-                        newUserInfos.add(userInfo);
+                ArrayList<Pill> newPillArrayList = new ArrayList<>();
+                for(Pill pill:arrayListPills){
+                    String name=pill.getName().toLowerCase();
+                    if(name.contains(newText)){
+                        newPillArrayList.add(pill);
                     }
                 }
-                customListAdapter.filterResult(newUserInfos);
+                customListAdapter.filterResult(newPillArrayList);
                 customListAdapter.notifyDataSetChanged();
                 return false;
             }
--- app/src/main/java/com/example/asifkhan/customlistview/adapters/CustomListAdapter.java
+++ app/src/main/java/com/example/asifkhan/customlistview/adapters/CustomListAdapter.java
@@ -12,22 +12,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.asifkhan.customlistview.R;
+import com.example.asifkhan.customlistview.models.Pill;
 import com.example.asifkhan.customlistview.models.UserInfo;
 
 import java.util.ArrayList;
 
 public class CustomListAdapter extends BaseAdapter implements View.OnClickListener{
-    private ArrayList<UserInfo> userInfos;
+    private ArrayList<Pill> pillArrayList;
     private Context context;
 
-    public CustomListAdapter(ArrayList<UserInfo> userInfos, Context context) {
-        this.userInfos = userInfos;
+    public CustomListAdapter(ArrayList<Pill> pillArrayList, Context context) {
+        this.pillArrayList = pillArrayList;
         this.context = context;
     }
 
     @Override
     public int getCount() {
-        return userInfos.size();
+        return pillArrayList.size();
     }
 
     @Override
@@ -44,57 +45,25 @@ public class CustomListAdapter extends BaseAdapter implements View.OnClickListen
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater layoutInflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view=layoutInflater.inflate(R.layout.custom_list_view_layout,null);
-        ImageView photo,option;
-        if(view==null){
-            photo=new ImageView(context);
-        }
-        UserInfo userInfo=userInfos.get(i);
-        photo=(ImageView)view.findViewById(R.id.photo);
-        option=(ImageView)view.findViewById(R.id.option);
+
+        Pill pill = pillArrayList.get(i);
         TextView name=(TextView)view.findViewById(R.id.name);
         TextView profession=(TextView)view.findViewById(R.id.profession);
-        photo.setImageResource(userInfo.getPhoto());
-        name.setText(userInfo.getName());
-        profession.setText(userInfo.getProfession());
-        option.setOnClickListener(this);
+
+        name.setText(pill.getName());
+        profession.setText(pill.getName());
         return view;
+    }
+
+    //file search result
+    public void filterResult(ArrayList<Pill> newPillArrayList){
+        pillArrayList=new ArrayList<>();
+        pillArrayList.addAll(newPillArrayList);
+        notifyDataSetChanged();
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.option:
-                showPopupMenu(view);
-                break;
-        }
-    }
 
-    // getting the popup menu
-    private void showPopupMenu(View view){
-        PopupMenu popupMenu=new PopupMenu(context,view);
-        popupMenu.getMenuInflater().inflate(R.menu.option_menu,popupMenu.getMenu());
-        popupMenu.show();
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.edit:
-                        Toast.makeText(context, "Edit !", Toast.LENGTH_SHORT).show();
-                        return true;
-                    case R.id.remove:
-                        Toast.makeText(context, "Remove !", Toast.LENGTH_SHORT).show();
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-        });
-    }
-
-    //file search result
-    public void filterResult(ArrayList<UserInfo> newUserInfos){
-        userInfos=new ArrayList<>();
-        userInfos.addAll(newUserInfos);
-        notifyDataSetChanged();
     }
 }
